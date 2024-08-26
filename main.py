665ccee6c273120648ee601b2304516e5fcf9de5
SirMrPickle <ehsgamer008@gmail.com>
2024-08-25 20:58:21 -0500

    main.py overhaul

--- main.py
+++ main.py
@@ -1,29 +1,47 @@
-import time, pyautogui, pytesseract
+import time
+import pyautogui
+import json
 
 def useItem(itemName):
-    pyautogui.leftClick(menuCoords)
+    coords = loadCoords()
+    pyautogui.leftClick(coords['menuCoords'])
     time.sleep(0.5)
-    pyautogui.leftClick(searchbarCoords)
+    pyautogui.leftClick(coords['searchbarCoords'])
     time.sleep(0.5)
     pyautogui.typewrite(itemName)
     time.sleep(0.5)
     pyautogui.press('enter')
     time.sleep(0.5)
-    pyautogui.leftClick(firstItemCoords)
+    pyautogui.leftClick(coords['firstItemCoords'])
 
 def teleportSpawn():
-    pyautogui.leftClick(teleporterCoords)
+    coords = loadCoords()
+    pyautogui.leftClick(coords['teleporterCoords'])
 
 def teleportBestArea():
-    return
+    # Implementation for finding and clicking the best area teleport goes here
+    pass
 
 def locationInit():
-    with open("config.txt", "w") as conf:
-        menuCoords = pyautogui.locateOnScreen("./images/menu.png", grayscale=True)
-        if menuCoords is not None: 
-            conf.write(str(menuCoords))  # Convert the coordinates to a string
-        else:
-            conf.write("no coords found")
+    coords = {}
+    coords['menuCoords'] = locateAndSaveCoords('./images/menu.png')
+    coords['searchbarCoords'] = locateAndSaveCoords('./images/searchbar.png')
+    coords['firstItemCoords'] = locateAndSaveCoords('./images/first_item.png')
+    coords['teleporterCoords'] = locateAndSaveCoords('./images/teleporter.png')
+    
+    with open("config.json", "w") as conf_file:
+        json.dump(coords, conf_file)
+
+def locateAndSaveCoords(image_path):
+    coords = pyautogui.locateOnScreen(image_path, grayscale=True)
+    if coords is not None:
+        return (coords.left, coords.top)  # Save as a tuple
+    else:
+        return "no coords found"
+
+def loadCoords():
+    with open("config.json", "r") as conf_file:
+        return json.load(conf_file)
 
 time.sleep(2)
 locationInit()
